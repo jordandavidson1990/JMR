41128a28b71e4082d189566bd39545064ba51f9d
Jordan Davidson <jordandavidson1990@hotmail.com>
2025-10-28 22:25:14 +0000

    Add link to footer

--- components/FooterEditorial.tsx
+++ components/FooterEditorial.tsx
@@ -1,41 +1,58 @@
-export default function FooterEditorial() {
-  return (
-    <footer className="bg-black text-gray-400 py-10 mt-16">
-      <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
-        <div>&copy; {new Date().getFullYear()} James Michael Rodgers</div>
-        <div className="mt-4 md:mt-0">
+const FooterEditorial = () => (
+  <footer className="bg-black text-gray-400 py-10 mt-16 border-t border-gray-800">
+    <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
+      <div className="flex flex-col items-center md:items-start space-y-1">
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} James Michael Rodgers. All Rights
+          Reserved.
+        </p>
+        <p className="text-xs text-gray-500">
+          Developed by:
           <a
-            href="https://www.instagram.com/jamesmichaelrodgers/"
-            className="text-gray-400 hover:text-red-600 mx-2 transition"
+            href="https://www.linkedin.com/in/jordan-davidson-1990/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-400 font-semibold hover:text-red-500 transition duration-200 ml-1"
           >
-            Instagram
+            Jordan Davidson
           </a>
-          <a
-            href="https://open.spotify.com/artist/2RC1s0mnZiQJp2Pm2bGnM4?si=aBe2zp6-Qf6jsQbp2wiJNQ"
-            className="text-gray-400 hover:text-red-600 mx-2 transition"
-          >
-            Spotify
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UCcB5jHP1cm9v7U94En4iboQ"
-            className="text-gray-400 hover:text-red-600 mx-2 transition"
-          >
-            YouTube
-          </a>
-          <a
-            href="https://www.facebook.com/jamesmichaelrodgers"
-            className="text-gray-400 hover:text-red-600 mx-2 transition"
-          >
-            Facebook
-          </a>
-          <a
-            href="https://tidal.com/artist/15693372"
-            className="text-gray-400 hover:text-red-600 mx-2 transition"
-          >
-            Tidal
-          </a>
-        </div>
+        </p>
       </div>
-    </footer>
-  );
-}
+
+      <div className="mt-6 md:mt-0 flex flex-wrap justify-center md:justify-end space-x-4">
+        <a
+          href="https://www.instagram.com/jamesmichaelrodgers/"
+          className="text-gray-400 hover:text-red-500 transition duration-200"
+        >
+          Instagram
+        </a>
+        <a
+          href="https://open.spotify.com/artist/2RC1s0mnZiQJp2Pm2bGnM4?si=aBe2zp6-Qf6jsQbp2wiJNQ"
+          className="text-gray-400 hover:text-red-500 transition duration-200"
+        >
+          Spotify
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCcB5jHP1cm9v7U94En4iboQ"
+          className="text-gray-400 hover:text-red-500 transition duration-200"
+        >
+          YouTube
+        </a>
+        <a
+          href="https://www.facebook.com/jamesmichaelrodgers"
+          className="text-gray-400 hover:text-red-500 transition duration-200"
+        >
+          Facebook
+        </a>
+        <a
+          href="https://tidal.com/artist/15693372"
+          className="text-gray-400 hover:text-red-500 transition duration-200"
+        >
+          Tidal
+        </a>
+      </div>
+    </div>
+  </footer>
+);
+
+export default FooterEditorial;
